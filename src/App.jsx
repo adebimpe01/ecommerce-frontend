9364f008382a641d4de866d2b64cafc707c5ac77
Okiki <abdulkareemrukayat18@gmail.com>
2026-03-23 15:34:25 +0100

    fix import issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
-import './App.css';
+import './App.css'
 
 function App() {
   const [cart, setCart] = useState([]);
 
   const loadCart = async () => {
-    try {
-      const response = await axios.get('/api/cart-items?expand=product');
-      setCart(response.data);
-    } catch (err) {
-      console.error('Failed to load cart:', err);
-    }
-  };
-
+  try {
+  const response = await axios.get('https://ecommerce-backend-kydf.onrender.com/api/cart-items?expand=product');
+    setCart(response.data);
+  } catch (error) {
+    console.error('Failed to load cart:', error);
+  }
+};
   useEffect(() => {
     loadCart();
   }, []);
@@ -26,11 +25,11 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage cart={cart} loadCart={loadCart} />} />
-      <Route path="checkout" element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
-      <Route path="orders" element={<OrdersPage cart={cart} />} />
-      <Route path="tracking" element={<TrackingPage />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} loadCart={loadCart} />} />
+      <Route path='orders' element={<OrdersPage cart={cart} />} />
+      <Route path='tracking' element={<TrackingPage />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App
